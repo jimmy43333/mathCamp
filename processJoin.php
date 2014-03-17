0ae9c43c85c392ab27082da091ab36b93ec384b7
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-17 13:30:22 +0800

    Fix the bug of PDO request.

--- processJoin.php
+++ processJoin.php
@@ -6,8 +6,50 @@ define('DB_HOST','localhost');
 define('DB_TYPE','mysql');
 $connect = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWD);
 $connect->query('GET NAMES UTF8');
-$result = $connect->prepare("INSERT into join_member values(:name, :nickname, :sex, :securityNumber, :phone, :cellphone, :address, :email, :school, :grade, :parentsName, :parentsRelation, :parentsPhone, :parentsAddress, :tshirtsize, :diet, :otherDietInfo, :illness, :introduction, :source, :addition");
-$where = array(':name' => $_GET['name'], ':nickname' => $_GET['nickname'], ':sex' => $_GET['sex'], ':securityNumber' => $_GET['securityNumber'], ':phone' => $_GET['phone'], ':cellphone' => $_GET['cellphone'], ':address' => $_GET['address'], ':email' => $_GET['email'], ':school' => $_GET['school'], ':grade' => $_GET['grade'], ':parentsName' => $_GET['parentsName'], ':parentsRelation' => $_GET['parentsRelation'], ':parentsPhone' => $_GET['parentsPhone'], ':parentsAddress' => $_GET['parentsAddress'], ':tshirtsize' => $_GET['tshirtsize'], ':diet' => $_GET['diet'], ':otherDietInfo' => $_GET['otherDietInfo'], ':illness' => $_GET['illness'], ':introduction' => $_GET['introduction'], ':source' => $_GET['source'], ':addition' => $_GET['addition']);
+$result = $connect->prepare("INSERT into join_member values(
+									:name, 
+									:nickname, 
+									:sex,
+									:securityNumber, 
+									:phone, 
+									:cellphone, 
+									:address, 
+									:email, 
+									:school, 
+									:grade, 
+									:parentsName, 
+									:parentsRelation, 
+									:parentsPhone, 
+									:parentsAddress, 
+									:tshirtsize, 
+									:diet, 
+									:otherDietInfo, 
+									:illness, 
+									:introduction, 
+									:source, 
+									:addition)");
+$where = array(
+			':name' => $_GET['name'], 
+			':nickname' => $_GET['nickname'], 
+			':sex' => $_GET['sex'], 
+			':securityNumber' => $_GET['securityNumber'], 
+			':phone' => $_GET['phone'], 
+			':cellphone' => $_GET['cellphone'], 
+			':address' => $_GET['address'],
+			':email' => $_GET['email'], 
+			':school' => $_GET['school'], 
+			':grade' => $_GET['grade'], 
+			':parentsName' => $_GET['parentsName'], 
+			':parentsRelation' => $_GET['parentsRelation'],
+			':parentsPhone' => $_GET['parentsPhone'], 
+			':parentsAddress' => $_GET['parentsAddress'],
+			':tshirtsize' => $_GET['tshirtsize'], 
+			':diet' => $_GET['diet'], 
+			':otherDietInfo' => $_GET['otherDietInfo'], 
+			':illness' => $_GET['illness'], 
+			':introduction' => $_GET['introduction'], 
+			':source' => $_GET['source'], 
+			':addition' => $_GET['addition']);
 $result->execute($where);
 echo 'yes';
 ?>
