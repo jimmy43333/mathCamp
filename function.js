48dbcc4302e0dd649b71efbe6c6c015c435baced
LarryStanley <Stanley@mcl46.mcl.math.ncu.edu.tw>
2014-03-18 20:43:34 +0800

    Maybe fix the bug.

--- function.js
+++ function.js
@@ -63,7 +63,7 @@ function validateForm(){
 	else
 		$('.securityNumber').addClass('has-success');
 	
-	return true;
+	return result;
 }
 
 function checkEmail(email) {
@@ -78,28 +78,25 @@ function checkEmail(email) {
 }
 
 function checkSecurityNumber(id) {
-  tab = "ABCDEFGHJKLMNPQRSTUVXYWZIO";              
-  A1 = new Array (1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3 );
-  A2 = new Array (0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5 );
-  Mx = new Array (9,8,7,6,5,4,3,2,1,1);
+	tab = "ABCDEFGHJKLMNPQRSTUVXYWZIO";              
+	A1 = new Array (1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3 );
+	A2 = new Array (0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5 );
+	Mx = new Array (9,8,7,6,5,4,3,2,1,1);
 
-  if ( id.length != 10 ) return false;
-  i = tab.indexOf( id.charAt(0) );
-  if ( i == -1 ) return false;
-  sum = A1[i] + A2[i]*9;
+	if ( id.length != 10 ) return false;
+	i = tab.indexOf( id.charAt(0) );
+	if ( i == -1 ) return false;
+	sum = A1[i] + A2[i]*9;
 
-  for ( i=1; i<10; i++ ) {
-      v = parseInt( id.charAt(i) );
-      if ( isNaN(v) ) return false;
-      sum = sum + v * Mx[i];
-    }
-  if ( sum % 10 != 0 ) 
+	for ( i=1; i<10; i++ ) {
+	  v = parseInt( id.charAt(i) );
+	  if ( isNaN(v) ) return false;
+	  sum = sum + v * Mx[i];
+	}
+	if ( sum % 10 != 0 ) 
 	  return false;
-  else{
-	  $('#securityNumberErrorMessage').empty();
+	else
 	  return true;
-	
-  }
 }
 
 function storageData(id){
@@ -156,8 +153,7 @@ function showFormThree(){
 
 function checkResult(){
 	var id = ['introduction', 'source'];
-	var idName = ['自我介紹', '訊息來源'];
-	if (checkForm(id, idName)){
+	if (validateForm()){
 		// Storage data
 		storageData(id);
 		allData['addition'] = $('#addition').val();
